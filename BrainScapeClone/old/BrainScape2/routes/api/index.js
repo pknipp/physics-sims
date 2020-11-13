12065a2587f8c373fc1136e82b3eb678e3ff6da8
Peter Knipp <peteraknipp@gmail.com>
2020-11-12 20:02:46 -0500

    Delete BrainScapeClone (because it was a git repo)

--- BrainScapeClone/old/BrainScape2/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const routes = ['csrf', 'session', 'users', "classes", "decks", "cards"];
-
-for (let route of routes) {
-  router.use(`/${route}`, require(`./${route}`));
-}
-
-module.exports = router;
