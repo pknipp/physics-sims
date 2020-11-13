063c0281b55efb079bdcfe94d39b38cdc5c400b5
Peter Knipp <peteraknipp@gmail.com>
2020-11-12 20:04:58 -0500

    Remove BrainScapeClone (because it's a git repo)

--- BrainScapeClone/old/5-18/twitter/refactored/front/src/store/middleware/thunk.js
+++
@@ -1,4 +0,0 @@
-const thunk = ({dispatch, getState}) => next => action =>{
-  return (typeof action === "function") ? action(dispatch, getState) : next(action);
-}
-export default thunk;
--- BrainScapeClone/old/nonRedux/routes/api/index.js
+++
@@ -1,4 +0,0 @@
-const router = require('express').Router();
-const routes = ['session', 'users', "classes", "decks", "cards"];
-routes.forEach(route => router.use(`/${route}`, require(`./${route}`)));
-module.exports = router;
