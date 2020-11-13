75c4ec8663e32f23756fdf70149e1c3cc023a40c
Peter Knipp <peteraknipp@gmail.com>
2020-11-12 20:03:46 -0500

    Remove BrainScapeClone (because it is a git repo)

--- BrainScapeClone/old/nonRedux/old/16BrainScape/twitter/refactored/front/src/store/reducers/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import tweetsReducer from './tweetsReducer';
-import usersReducer from './usersReducer';
-import authReducer from '../authentication';
-
-export default combineReducers({ tweets: tweetsReducer, users: usersReducer, auth: authReducer });
