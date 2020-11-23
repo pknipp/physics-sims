fc051b28edb9bf7aa7afabd3389f795db306b134
Peter Knipp <peteraknipp@gmail.com>
2020-11-22 21:35:31 -0500

    Solve the heat equation and display answers as a list

--- client/src/Container.js
+++ client/src/Container.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Logout from "./components/Logout";
 import Collection from "./components/Collection";
 import Asteroids from "./components/Asteroids";
+import Heat from "./components/Heat";
 import DrawArea from "./components/DrawArea";
 import Account from "./components/Account";
 
@@ -19,6 +20,7 @@ class Container extends React.Component {
             <span><NavLink className="nav" exact to="/"       activeClassName="active">Home           </NavLink></span>
             <span><NavLink className="nav" to="/drum" activeClassName="active">Drumhead</NavLink></span>
             <span><NavLink className="nav" to="/asteroids" activeClassName="active">Asteroid field</NavLink></span>
+            <span><NavLink className="nav" to="/heat" activeClassName="active">Heat equation</NavLink></span>
             <span><NavLink className="nav" to="/draw" activeClassName="active">DrawArea</NavLink></span>
             <span><NavLink className="nav" to="/account"      activeClassName="active">Account details</NavLink></span>
             <span><NavLink className="nav" to="/logout"       activeClassName="active">Logout         </NavLink></span>
@@ -28,6 +30,7 @@ class Container extends React.Component {
           <Route path="/logout"       component={Logout}     />
           <Route path="/drum"       component={Collection}     />
             <Route path="/asteroids" component={Asteroids} />
+            <Route path="/heat" component={Heat} />
             <Route path="/draw" component={DrawArea} />
           <Route path="/account"      component={Account}    />
         </Switch>
--- client/src/components/Heat.js
+++ client/src/components/Heat.js
@@ -5,7 +5,14 @@ class Heat extends React.Component {
         this.state = {
             running: false,
             time: 0,
-            n: 100,
+            n: 5,
+            Ts: [],
+            leftIns: true,
+            rightIns: true,
+            leftT: 0.2,
+            rightT: 0.8,
+            alpha: 10,
+            dt: 100,
         }
     }
 
@@ -24,5 +31,47 @@ class Heat extends React.Component {
         this.setState({ Ts });
     }
 
+    nextTs = _ => {
+        let { Ts, alpha, n, leftIns, rightIns, leftT, rightT } = this.state
+        let nextTs = [];
+        nextTs.push(Ts[1]/alpha + Ts[0] * (1 - 2/alpha) + ((leftIns) ? Ts[0] : leftT)/alpha);
+        for (let i = 1; i < n - 1; i++) {
+            nextTs.push((Ts[i + 1] + Ts[i - 1])/alpha + Ts[i] * (1 - 2/alpha));
+        }
+        nextTs.push(Ts[n - 2]/alpha + Ts[n - 1] * (1 - 2/alpha) + ((rightIns) ? Ts[n - 1] : rightT)/alpha);
+        this.setState({ Ts: nextTs });
+    }
+
+    toggle = () => {
+        const running = !this.state.running;
+        if (running) {
+          this.interval = setInterval(this.tick, this.state.dt);
+          this.setState({time: 0});
+        } else {
+          clearInterval(this.interval);
+          this.interval = null;
+        }
+        this.setState({ running });
+    };
+
+    render() {
+        let rows = this.state.Ts.map(T => <li>{T}</li>);
+        return (
+            <>
+            <span className="button-container">
+                <button onClick={this.toggle}>
+                    {this.state.running ? "Pause" : "Run"}
+                </button>
+            </span>
+            <span>
+            time: {Math.round(100 * this.state.time)/100} s
+            </span>
+            <ul>
+                {rows}
+            </ul>
+            </>
+        )
+    }
+}
 
 export default Heat;
