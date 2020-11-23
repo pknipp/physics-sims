a44f09dd2a8faf13040c17a0e4848efe8b6adf65
Peter Knipp <peteraknipp@gmail.com>
2020-11-23 18:44:07 -0500

    Prepare to change to signup branch.

--- client/src/components/Heat.js
+++ client/src/components/Heat.js
@@ -10,10 +10,10 @@ class Heat extends React.Component {
             rightIns: false,
             leftT: 0.0,
             rightT: 0.0,
-            alpha: 1,
+            alpha: 0.1,
             logN: 1,
             n: 1,
-            dt: 1000,
+            dt: 100,
         }
     }
 
@@ -32,16 +32,27 @@ class Heat extends React.Component {
 
     makeDist = _ => {
         let Ts = [];
+        let coef = [];
+        for (let m = 1; m < 10; m++) {
+            coef.push(Math.random());
+        }
         for (let i = 0; i < this.state.n; i++) {
             Ts.push(1);
             // Ts.push(Math.random());
+            // Ts.push(Math.sin(Math.PI*i/this.state.n));
+            let T = 0;
+            for (let m = 1; m < 10; m++) {
+                T += coef[m] * Math.sin(Math.PI * m * i/this.state.n);
+            }
+            // Ts.push(Math.min(0,T));
         }
         this.setState({ Ts });
     }
 
     tridiag = _ => {
         let { n, Ts, leftIns, leftT, rightIns, rightT } = this.state;
-        let alpha0 = this.state.alpha * this.state.dt * n * n / 1000;
+        debugger
+        let alpha0 = this.state.alpha * this.state.dt * (n + 1) * (n + 1) / 1000;
         let a = new Array(n).fill(-alpha0/2);
         let b = new Array(n).fill(1 + alpha0);
         let c = new Array(n).fill(-alpha0/2);
@@ -50,19 +61,28 @@ class Heat extends React.Component {
         let g = new Array(n);
         if (this.state.leftIns ) b[0]     -= alpha0/2;
         if (this.state.rightIns) b[n - 1] -= alpha0/2;
-        r[0]  = (1 - alpha0) * Ts[0]  + (Ts[1] + (leftIns ? Ts[0] : leftT)) * alpha0 / 2;
-        r[n-1]= (1 - alpha0) * Ts[n-1]+ (Ts[n-2]+(rightIns? Ts[n-1]: rightT))*alpha0/2;
+        // r[0]  = (1 - alpha0) * Ts[0]  + (Ts[1] + (leftIns ? Ts[0] : leftT)) * alpha0 / 2;
+        r[0]  = Ts[0]  + ((Ts[1] - Ts[0]) + ((leftIns ? Ts[0] : leftT) - Ts[0])) * alpha0 / 2;
+        // r[n-1]= (1 - alpha0) * Ts[n-1]+ (Ts[n-2]+(rightIns? Ts[n-1]: rightT))*alpha0/2;
+        r[n-1]= Ts[n-1]+((Ts[n-2]-Ts[n-1])+((rightIns ? Ts[n-1]:rightT)-Ts[n-1]))*alpha0 / 2;
         let bet = b[0];
-        u[0] = r[0];
-        for (let i = 1; i < n - 2; i++) {
+        u[0] = r[0]/bet;
+        debugger
+        for (let i = 1; i < n; i++) {
             g[i] = c[i - 1] / bet;
             bet = b[i] - a[i] * g[i];
-            r[i] = Ts[i] + (Ts[i - 1] - Ts[i] - Ts[i] + Ts[i + 1]) * alpha0 / 2;
+            if (i < n - 1) r[i] = Ts[i] + ((Ts[i - 1] - Ts[i]) - (Ts[i] - Ts[i + 1])) * alpha0 / 2;
             u[i] = (r[i] - a[i] * u[i - 1]) / bet;
         }
+        g[n - 1] = c[n - 2]/bet;
+        u[n - 1] = (r[n - 1] - a[n - 1] * u[n - 2])/bet;
+        debugger
         for (let i = n - 2; i >= 0; i--) {
             u[i] -= g[i + 1] * u[i + 1];
+            // I thought that the following may make things more stable, to no avail
+            // u[i] = Math.min(0, u[i]);
         }
+        debugger
         this.setState({Ts: u});
     }
 
@@ -96,8 +116,9 @@ class Heat extends React.Component {
     };
 
     render() {
-        let bars = this.state.Ts.map(T => (
-            <div className="bar"
+        let bars = this.state.Ts.map((T, idx) => (
+            <div key={`${idx}`}
+                className="bar"
                 style={{
                 height:`${Math.round(600*T)}px`,
                 width:`${Math.round(1000/this.state.n)}px`,
